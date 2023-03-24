3da167ae4cef38b5e56c51fc5789e42d8a87a67b
ragulhpSPK <ragulhp27@gmail.com>
2023-03-24 18:19:16 +0530

    edit553

--- src/App.js
+++ src/App.js
@@ -1,203 +1,60 @@
 import "./App.css";
+import { ContentHomepage } from "./heper/contenthome";
 
 function App() {
   return (
-    <div className="h-auto w-[screen] overflow-x-hidden">
-      <div>
-        <div className="text-center h-16 mt-5 text-4xl font-bold text-slate-400">
-          <h1>Home Page Template Designs</h1>
-        </div>
-        <div className=" sm:grid sm:grid-cols-2 xl:grid xl:grid-cols-3 xsm:ml-40 gap-10">
-          <div className="lg:ml-28">
-            <h1 className="text-2xl pt-5">Template 01</h1>
-            <p className="text-2xl pt-5">Using React with Tailwind Css</p>
-            <iframe
-              src="https://home-template.vercel.app/"
-              style={{ height: "500px", width: "320px" }}
-              title="Iframe Example"
-              className="pt-5"
-              scrolling="no"
-            ></iframe>
-            <div className="flex w-[500px]">
-              <div>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke-width="1.5"
-                  stroke="currentColor"
-                  className="w-6 h-10"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75"
-                  />
-                </svg>
-              </div>
-              <div>
-                <a
-                  href="https://home-template.vercel.app/"
-                  className="text-2xl pt-5"
-                >
-                  Clik Here for visit this page
-                </a>
-              </div>
-            </div>
-          </div>
-
-          <div className="lg:ml-28">
-            <h1 className="text-2xl pt-5">Template 02</h1>
-            <p className="text-2xl pt-5">Using React with Tailwind Css</p>
-            <iframe
-              src="https://template-01-three.vercel.app/"
-              style={{ height: "500px", width: "320px" }}
-              title="Iframe Example"
-              className="pt-5"
-              scrolling="no"
-            ></iframe>
-            <div className="flex w-[500px]">
-              <div>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke-width="1.5"
-                  stroke="currentColor"
-                  className="w-6 h-10"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75"
-                  />
-                </svg>
-              </div>
-              <div>
-                <a
-                  href="https://template-01-three.vercel.app/"
-                  className="text-2xl pt-5"
-                >
-                  Clik Here for visit this page
-                </a>
+    <div className="h-auto w-[100vw] xsm:pl-1  overflow-x-hidden">
+      <div
+        className="text-center h-16 mt-5 text-4xl font-bold text-slate-700"
+        id="writer"
+      >
+        <h1 className="text">Home Page Template Designs</h1>
+      </div>
+      <div className="xsm:grid xsm:grid-cols-1 xsm:w-min sm:grid sm:grid-cols-2 xl:grid xl:grid-cols-3 xsm:ml-40 gap-10">
+        {ContentHomepage.map((data, index) => {
+          return (
+            <div key={data.id}>
+              <div
+                className="lg:ml-28 xsm:text-center"
+                data-aos="zoom-in"
+                data-aos-duration="1000"
+              >
+                <h1 className="text-2xl pt-5">{data.heading}</h1>
+                <p className="text-2xl pt-5">{data.content}</p>
+                <iframe
+                  src={data.url}
+                  style={{ height: "420px", width: "320px" }}
+                  title="Iframe Example"
+                  className="pt-5"
+                  scrolling="no"
+                ></iframe>
+                <div className="flex w-[500px]">
+                  <div>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke-width="1.5"
+                      stroke="currentColor"
+                      className="w-6 h-10"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75"
+                      />
+                    </svg>
+                  </div>
+                  <div>
+                    <a href={data.url} className="text-2xl pt-5">
+                      Clik Here for visit this page
+                    </a>
+                  </div>
+                </div>
               </div>
             </div>
-          </div>
-
-          <div className="lg:ml-28">
-            <h1 className="text-2xl pt-5">Template 03</h1>
-            <p className="text-2xl pt-5">Using React with Tailwind Css</p>
-            <iframe
-              src="https://template-02-one.vercel.app/"
-              style={{ height: "500px", width: "320px" }}
-              title="Iframe Example"
-              className="pt-5"
-              scrolling="no"
-            ></iframe>
-            <div className="flex w-[500px]">
-              <div>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke-width="1.5"
-                  stroke="currentColor"
-                  className="w-6 h-10"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75"
-                  />
-                </svg>
-              </div>
-              <div>
-                <a
-                  href="https://template-02-one.vercel.app/"
-                  className="text-2xl pt-5"
-                >
-                  Clik Here for visit this page
-                </a>
-              </div>
-            </div>
-          </div>
-
-          <div className="lg:ml-28">
-            <h1 className="text-2xl pt-5">Template 04</h1>
-            <p className="text-2xl pt-5">Using React with Tailwind Css</p>
-            <iframe
-              src="https://template-03.vercel.app/"
-              style={{ height: "500px", width: "320px" }}
-              title="Iframe Example"
-              className="pt-5"
-              scrolling="no"
-            ></iframe>
-            <div className="flex w-[500px]">
-              <div>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke-width="1.5"
-                  stroke="currentColor"
-                  className="w-6 h-10"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75"
-                  />
-                </svg>
-              </div>
-              <div>
-                <a
-                  href="https://template-03.vercel.app/"
-                  className="text-2xl pt-5"
-                >
-                  Clik Here for visit this page
-                </a>
-              </div>
-            </div>
-          </div>
-
-          <div className="lg:ml-28">
-            <h1 className="text-2xl pt-5">Template 05</h1>
-            <p className="text-2xl pt-5">Using React with Tailwind Css</p>
-            <iframe
-              src="https://template-05.vercel.app/"
-              style={{ height: "500px", width: "320px" }}
-              title="Iframe Example"
-              className="pt-5"
-              scrolling="no"
-            ></iframe>
-            <div className="flex w-[500px]">
-              <div>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke-width="1.5"
-                  stroke="currentColor"
-                  className="w-6 h-10"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75"
-                  />
-                </svg>
-              </div>
-              <div>
-                <a
-                  href="https://template-05.vercel.app/"
-                  className="text-2xl pt-5"
-                >
-                  Clik Here for visit this page
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
+          );
+        })}
       </div>
     </div>
   );
---
+++ src/heper/contenthome.js
@@ -0,0 +1,38 @@
+export const ContentHomepage = [
+  {
+    id: "1",
+    heading: "Template 01",
+    content: "Using React with Tailwind Css",
+    url: "https://home-template.vercel.app/",
+  },
+  {
+    id: "2",
+    heading: "Template 02",
+    content: "Using React with Tailwind Css",
+    url: "https://template-01-three.vercel.app/",
+  },
+  {
+    id: "3",
+    heading: "Template 03",
+    content: "Using React with Tailwind Css",
+    url: "https://template-02-one.vercel.app/",
+  },
+  {
+    id: "4",
+    heading: "Template 04",
+    content: "Using React with Tailwind Css",
+    url: "https://template-03.vercel.app/",
+  },
+  {
+    id: "5",
+    heading: "Template 05",
+    content: "Using React with Tailwind Css",
+    url: "https://template-05.vercel.app/",
+  },
+  {
+    id: "6",
+    heading: "Template 06",
+    content: "Using React with Tailwind Css",
+    url: "https://template-06.vercel.app/",
+  },
+];
